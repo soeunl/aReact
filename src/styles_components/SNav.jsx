11bce73795cc1899f412f02c9cfeca50770e8838
soeunl <soeun318@gmail.com>
2024-03-10 18:00:52 +0900

    tow commit

--- src/styles_components/SNav.jsx
+++ src/styles_components/SNav.jsx
@@ -1,14 +1,40 @@
 import React from 'react'
 
+// 파라미터 유무
+// 파라미터가 있다면 () => {} 화살표 함수를 사용
+// 파라미터가 없다면 그냥 호출
+
 const SNav = () => {
+    const onMouseOverHandle = () => {
+        console.log('mouse Over')
+    }
+    const onMouseOutHandle = () => {
+        console.log('mouse Out')
+    }
     return (
         <nav className="lnb">
             <ul>
                 <li>
-                    <a href="#">HOME</a>
+                    <a
+                        href="#"
+                        onMouseOver={() => {
+                            console.log('마우스오버')
+                        }}
+                        onMouseOut={() => {
+                            console.log('마우스아웃')
+                        }}
+                    >
+                        HOME
+                    </a>
                 </li>
                 <li>
-                    <a href="#">HTML</a>
+                    <a
+                        href="#"
+                        onMouseOver={onMouseOverHandle}
+                        onMouseOut={onMouseOutHandle}
+                    >
+                        HTML
+                    </a>
                 </li>
                 <li>
                     <a href="#">CSS</a>
